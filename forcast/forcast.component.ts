6c32046c68a52446d0cbdc7faa35d83468007e62
Arshad-67 <128599071+Arshad-67@users.noreply.github.com>
2023-05-22 21:10:39 -1200

    Add files via upload

---
+++ forcast/forcast.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Data } from '@angular/router';
+
+@Component({
+  selector: 'app-forcast',
+  templateUrl: './forcast.component.html',
+  styleUrls: ['./forcast.component.css']
+})
+export class ForcastComponent {
+  
+
+ 
+ 
+}
